eeb0b87db37f3b7a442c92cdd0c0f6947546130c
Brentani, Romano Fabiano (STUDENTS) <romano.brentani@students.unibe.ch>
2023-11-12 14:15:34 +0100

    implemented thum down and thumb up detection

---
+++ handtracker.py
@@ -0,0 +1,164 @@
+import cv2
+import mediapipe as mp
+import mediapipe.python.solutions.drawing_styles as drawing_styles
+
+mp_hands = mp.solutions.hands
+hand = mp_hands.Hands()
+mp_drawing = mp.solutions.drawing_utils
+
+
+def is_thumb_up(results, img):
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(img,
+                                      hand_landmarks,
+                                      mp_hands.HAND_CONNECTIONS,
+                                      drawing_styles.get_default_hand_landmarks_style(),
+                                      drawing_styles.get_default_hand_connections_style()
+                                      )
+
+            thumb_left_up = thumb_is_up_left(hand_landmarks)
+            thumb_right_up = thumb_is_up_right(hand_landmarks)
+            hand_open_left = hand_open_thumb_left(hand_landmarks)
+            hand_open_right = hand_open_thumb_right(hand_landmarks)
+
+            if thumb_left_up and not hand_open_left:
+                print("thumb left")
+                return True
+            if thumb_right_up and not hand_open_right:
+                print("thumb right")
+                return True
+            if only_thumb_up(hand_landmarks):
+                print("only thumb")
+                return True
+
+        return False
+
+
+def thumb_is_up_right(hand_landmarks):
+    return (hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x > hand_landmarks.landmark[
+        mp_hands.HandLandmark.INDEX_FINGER_DIP].x and
+            hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP].x > hand_landmarks.landmark[
+                mp_hands.HandLandmark.MIDDLE_FINGER_DIP].x and
+            hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP].x > hand_landmarks.landmark[
+                mp_hands.HandLandmark.RING_FINGER_DIP].x and
+            hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP].x > hand_landmarks.landmark[
+                mp_hands.HandLandmark.PINKY_DIP].x and only_thumb_up(hand_landmarks))
+
+
+def thumb_is_up_left(hand_landmarks):
+    return (hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x < hand_landmarks.landmark[
+        mp_hands.HandLandmark.INDEX_FINGER_MCP].x and
+            hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x < hand_landmarks.landmark[
+                mp_hands.HandLandmark.MIDDLE_FINGER_MCP].x and
+            hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x < hand_landmarks.landmark[
+                mp_hands.HandLandmark.RING_FINGER_MCP].x and
+            hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x < hand_landmarks.landmark[
+                mp_hands.HandLandmark.PINKY_MCP].x and only_thumb_up(hand_landmarks))
+
+
+def hand_open_thumb_left(hand_landmarks):
+    return (hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_PIP].x < hand_landmarks.landmark[
+        mp_hands.HandLandmark.INDEX_FINGER_DIP].x or
+            hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_PIP].x < hand_landmarks.landmark[
+                mp_hands.HandLandmark.MIDDLE_FINGER_DIP].x or hand_landmarks.landmark[
+                mp_hands.HandLandmark.RING_FINGER_PIP].x < hand_landmarks.landmark[
+                mp_hands.HandLandmark.RING_FINGER_DIP].x or
+            hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_PIP].x < hand_landmarks.landmark[
+                mp_hands.HandLandmark.PINKY_DIP].x)
+
+
+def hand_open_thumb_right(hand_landmarks):
+    if (thumb_is_up_right(hand_landmarks) and
+            (hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].x < hand_landmarks.landmark[
+                mp_hands.HandLandmark.INDEX_FINGER_PIP].x or
+             hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP].x < hand_landmarks.landmark[
+                 mp_hands.HandLandmark.MIDDLE_FINGER_PIP].x or
+             hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_DIP].x < hand_landmarks.landmark[
+                 mp_hands.HandLandmark.RING_FINGER_PIP].x or
+             hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_DIP].x < hand_landmarks.landmark[
+                 mp_hands.HandLandmark.PINKY_PIP].x)):
+        return True
+    return False
+
+
+def only_thumb_up(hand_landmarks):
+    return (
+            hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y < hand_landmarks.landmark[
+        mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y and
+            hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y < hand_landmarks.landmark[
+                mp_hands.HandLandmark.RING_FINGER_TIP].y and
+            hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y < hand_landmarks.landmark[
+                mp_hands.HandLandmark.PINKY_MCP].y
+            and hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_IP].y > hand_landmarks.landmark[
+                mp_hands.HandLandmark.THUMB_TIP].y)
+
+
+def is_thumb_down(results, img):
+    fingers_idx = [
+        mp_hands.HandLandmark.INDEX_FINGER_DIP, mp_hands.HandLandmark.MIDDLE_FINGER_DIP,
+        mp_hands.HandLandmark.RING_FINGER_DIP, mp_hands.HandLandmark.PINKY_DIP
+    ]
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(img,
+                                      hand_landmarks,
+                                      mp_hands.HAND_CONNECTIONS,
+                                      drawing_styles.get_default_hand_landmarks_style(),
+                                      drawing_styles.get_default_hand_connections_style()
+                                      )
+
+            for finger in fingers_idx:
+                if (hand_landmarks.landmark[finger].x > hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x
+                        or not hand_closed(hand_landmarks)):
+                    return False
+
+    return True
+
+
+def hand_closed(hand_landmarks):
+    fingers_idx_TIP = [
+        mp_hands.HandLandmark.INDEX_FINGER_TIP, mp_hands.HandLandmark.MIDDLE_FINGER_TIP,
+        mp_hands.HandLandmark.RING_FINGER_TIP, mp_hands.HandLandmark.PINKY_TIP
+    ]
+
+    fingers_idx_PIP = [
+        mp_hands.HandLandmark.INDEX_FINGER_PIP, mp_hands.HandLandmark.MIDDLE_FINGER_PIP,
+        mp_hands.HandLandmark.RING_FINGER_PIP, mp_hands.HandLandmark.PINKY_PIP
+    ]
+
+    for finger_TIP, finger_PIP in zip(fingers_idx_TIP, fingers_idx_PIP):
+        print("TIP", hand_landmarks.landmark[finger_TIP].x)
+        print("PIP", hand_landmarks.landmark[finger_PIP].x)
+        if hand_landmarks.landmark[finger_TIP].x < hand_landmarks.landmark[finger_PIP].x:
+            return False
+
+    return True
+
+
+def startDetection():
+    capture = cv2.VideoCapture(0)
+
+    while True:
+        ret, img = capture.read()
+
+        if not ret:
+            continue
+
+        rgb_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hand.process(rgb_frame)
+
+        thumb_is_up = is_thumb_up(results, img)
+        if thumb_is_up:
+            print("thumb up!")
+
+        thumb_is_down = is_thumb_down(results, img)
+        if thumb_is_down:
+            print("thumb is down")
+
+        cv2.imshow('Hand Recognition', img)
+        cv2.waitKey(0)
+
+    capture.release()
+    cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+import handtracker as hd
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
@@ -11,7 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Jan')
-    print_hi('Roman')
+    hd.startDetection()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
